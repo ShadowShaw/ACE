4cdc4a10bf5f1821d62ed04f25848563d31f7725
Martin Minařík <shaadow@volny.cz>
2013-10-26 22:41:03 +0200

    bootstrap

--- WebAgent/Controllers/AccountController.cs
+++ WebAgent/Controllers/AccountController.cs
@@ -45,7 +45,7 @@ namespace ACEAgent.Controllers
             }
 
             // If we got this far, something failed, redisplay form
-            ModelState.AddModelError("", "The user name or password provided is incorrect.");
+            ModelState.AddModelError("", "Uživatelské jméno nebo heslo není platné.");
             return View(model);
         }
 
