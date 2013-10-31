6b491ad5282e95e106597edc42530bb82f3ccd8e
Martin Minařík <shaadow@volny.cz>
2013-11-01 00:25:33 +0100

    moving to bootstrap completed

--- WebAgent/App_Start/BundleConfig.cs
+++ WebAgent/App_Start/BundleConfig.cs
@@ -26,6 +26,20 @@ namespace ACEAgent
                             .Include("~/Scripts/jquery-{version}.js")
                             .Include("~/Scripts/jquery-ui-{version}.js")
                             .Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
+                        "~/Content/themes/base/jquery.ui.core.css",
+                        "~/Content/themes/base/jquery.ui.resizable.css",
+                        "~/Content/themes/base/jquery.ui.selectable.css",
+                        "~/Content/themes/base/jquery.ui.accordion.css",
+                        "~/Content/themes/base/jquery.ui.autocomplete.css",
+                        "~/Content/themes/base/jquery.ui.button.css",
+                        "~/Content/themes/base/jquery.ui.dialog.css",
+                        "~/Content/themes/base/jquery.ui.slider.css",
+                        "~/Content/themes/base/jquery.ui.tabs.css",
+                        "~/Content/themes/base/jquery.ui.datepicker.css",
+                        "~/Content/themes/base/jquery.ui.progressbar.css",
+                        "~/Content/themes/base/jquery.ui.theme.css"));
         }
 
         //// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
@@ -58,19 +72,7 @@ namespace ACEAgent
         //                    .Include("~/Scripts/jquery-ui-{version}.js")
         //                    .Include("~/Scripts/bootstrap.js"));
 
-        //    bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-        //                "~/Content/themes/base/jquery.ui.core.css",
-        //                "~/Content/themes/base/jquery.ui.resizable.css",
-        //                "~/Content/themes/base/jquery.ui.selectable.css",
-        //                "~/Content/themes/base/jquery.ui.accordion.css",
-        //                "~/Content/themes/base/jquery.ui.autocomplete.css",
-        //                "~/Content/themes/base/jquery.ui.button.css",
-        //                "~/Content/themes/base/jquery.ui.dialog.css",
-        //                "~/Content/themes/base/jquery.ui.slider.css",
-        //                "~/Content/themes/base/jquery.ui.tabs.css",
-        //                "~/Content/themes/base/jquery.ui.datepicker.css",
-        //                "~/Content/themes/base/jquery.ui.progressbar.css",
-        //                "~/Content/themes/base/jquery.ui.theme.css"));
+            
         //}
     }
 }
